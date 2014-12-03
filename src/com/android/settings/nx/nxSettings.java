0da5c21ab4516bedab6654aee9d1df258c19ac9d
AragaoAnderson <andersonaragao@me.com>
2014-12-03 21:06:00 -0200

    Settings: Invert Quick settings on lock screen [2/2]

--- src/com/android/settings/nx/nxSettings.java
+++ src/com/android/settings/nx/nxSettings.java
@@ -86,7 +86,7 @@ public class nxSettings extends SettingsPreferenceFragment
         mBlockOnSecureKeyguard = (SwitchPreference) prefs.findPreference(PREF_BLOCK_ON_SECURE_KEYGUARD);
         if (lockPatternUtils.isSecure()) {
             mBlockOnSecureKeyguard.setChecked(Settings.Secure.getInt(getContentResolver(),
-                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD, 1) == 1);
+                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD, 0) == 1);
             mBlockOnSecureKeyguard.setOnPreferenceChangeListener(this);
         } else {
             statusbarCategory.removePreference(mBlockOnSecureKeyguard);
